3226149bba4b2d6c89d35c1a7abddfe04482b847
HuangWei <market@zuiyue.com>
2024-01-26 12:17:11 +0800

    init finished

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+#[macro_use]
+extern crate wei_log;
+
+use wei_single::SingleInstance;
+
+use job_scheduler::{JobScheduler, Job};
+use std::time::Duration;
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn parse_line(line: &str) -> (String, String) {
+    let parts: Vec<&str> = line.split_whitespace().collect();
+    let parts_one = parts[0..6].join(" ");
+    let parts_two = parts[6..].join(" ");
+
+    (parts_one, parts_two)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    wei_windows::init();
+
+    let instance = SingleInstance::new("wei-scheduler")?;
+    if !instance.is_single() { 
+        std::process::exit(1);
+    };
+
+    let path = Path::new("crontab.txt");
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut sched = JobScheduler::new();
+
+    for line in reader.lines() {
+        let line = line?;
+
+        if line.trim().is_empty() {
+            continue;
+        }
+
+        let (cron_expr, command) = parse_line(&line);
+
+        let command = if cfg!(target_os = "windows") {
+            format!("cmd.exe /c {}", command)
+        } else {
+            format!("sh -c {}", command)
+        };
+
+        sched.add(Job::new(cron_expr.parse()?, move || {
+            let command: Vec<&str> = command.split_whitespace().collect();
+            info!("command: {:?}", command);
+            let output = wei_run::command(command[0], command[1..].to_vec()).unwrap();
+            info!("output: {:?}", output);
+        }));
+    }
+
+    loop {
+        sched.tick();
+        std::thread::sleep(Duration::from_millis(500));
+    }
+}
